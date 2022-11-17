18236435c5aefb9e46f1e8d709bb9458b8dfd17a
김빛누리 <bnurik104@gmail.com>
2022-11-18 01:55:16 +0900

    add: 회원정보 수정 페이지 추가
    - master branch에서 pull함
    
    
    (cherry picked from commit 5619b8fe22225f2f69fae193297aca7bb2004f82)

---
+++ src/components/login/InterestSelectBox.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="row" id="deal-header"> 
+            <div class="col-1 mb-2"> 서울 > </div>
+            <div class="col-1 mb-2 me-4">
+                <select id="gugun-select" class="form-select form-select-sm" aria-label=".form-select-sm example">
+                <option value="11110">종로구</option>
+                <option value="11140">중구</option>
+                <option value="11170">용산구</option>
+                <option  value="11200">성동구</option>
+                <option  value="11215">광진구</option>
+                <option  value="11230">동대문구</option>
+                <option  value="11260">중랑구</option>
+                <option  value="11290">성북구</option>
+                <option  value="11305">강북구</option>
+                <option  value="11320">도봉구</option>
+                <option  value="11350">노원구</option>
+                <option value="11380">은평구</option>
+                <option value="11410">서대문구</option>
+                <option  value="11440">마포구</option>
+                <option  value="11470">양천구</option>
+                <option  value="11500">강서구</option>
+                <option  value="11530">구로구</option>
+                <option  value="11545">금천구</option>
+                <option  value="11560">영등포구</option>
+                <option  value="11590">동작구</option>
+                <option  value="11620">관악구</option>
+                <option  value="11650">서초구</option>
+                <option  value="11680">강남구</option>
+                <option  value="11710">송파구</option>
+                <option  value="11740">강동구</option>
+                </select>
+			</div>
+			<div class="col-1 mb-2 me-4">
+                <select id="dong-select" class="form-select form-select-sm" aria-label=".form-select-sm example">
+                    ...
+                </select>
+			</div>
+		</div>
+
+        
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/AppMyPage.vue
@@ -0,0 +1,64 @@
+<template>
+        <div class="container">
+            <div class="pb-5 pt-5 mb-3" ></div>
+
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+                <input type="email" id="registerEmail" class="form-control" readonly/>
+                <label class="form-label" for="registerEmail">Email</label>
+            </div>
+
+            <!-- Username input -->
+            <div class="form-outline mb-4">
+                <input type="text" id="registerUsername" class="form-control" />
+                <label class="form-label" for="registerUsername">Username</label>
+            </div>
+
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+                <input type="password" id="registerPassword" class="form-control" />
+                <label class="form-label" for="registerPassword">Password</label>
+            </div>
+
+            <!-- Repeat Password input -->
+            <div class="form-outline mb-4">
+                <input type="password" id="registerRepeatPassword" class="form-control" />
+                <label class="form-label" for="registerRepeatPassword">Repeat password</label>
+            </div>
+
+            <!-- Checkbox -->
+            <div class="form-check">
+                <input class="form-check-input me-2" type="checkbox" value="" id="interestCheck" v-model="setInterestArea"/>
+                <label class="form-check-label" for="interestCheck">
+                    관심지역 설정
+                </label>
+                
+            </div>
+            <div v-show="setInterestArea">
+                다대포~~~~
+                <interest-select-box></interest-select-box>
+            </div>
+
+            <!-- Submit button -->
+            <button class="btn btn-primary btn-block mb-3">Sign in</button>
+
+        </div>
+</template>
+
+<script>
+import InterestSelectBox from "@/components/login/InterestSelectBox.vue";
+export default {
+    data: function () {
+        return{
+            setInterestArea: false
+        }
+    },
+    components: {
+        InterestSelectBox,
+    }
+}
+</script>
+
+<style>
+
+</style>
