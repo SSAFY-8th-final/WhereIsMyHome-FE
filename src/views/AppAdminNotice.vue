b02c7fac83271fcb6a275d21390ff734ec014562
김아현 <mauv2sky@gmail.com>
2022-11-17 17:59:15 +0900

    add. 관리자 공지사항 모달 컴포넌트

---
+++ src/views/AppAdminNotice.vue
@@ -0,0 +1,16 @@
+<template>
+  <AdminNotice></AdminNotice>
+</template>
+
+<script>
+import AdminNotice from "@/components/admin/AdminNotice.vue"
+export default {
+    components: {
+        AdminNotice
+    }
+}
+</script>
+
+<style>
+
+</style>
