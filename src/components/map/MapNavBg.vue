c658c4618c1e58781eb45161de8a56b9372a7100
김빛누리 <bnurik104@gmail.com>
2022-11-18 11:30:29 +0900

    design: 지도 사이드 패널 searchBar 추가 및 기타 디자인 수정
    - 지도용 검색 박스 컴포넌트 추가
    - style2.css 파일 정리

---
+++ src/components/map/MapNavBg.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- ======= MapNavBg ======= -->
+    <div class="breadcrumbs d-flex align-items-center" style="background-image: url('assets/img/breadcrumbs-bg.jpg');">
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/map/SearchBarMap.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <form id="searchBar">
+      <div class="mb-3">
+      <label>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="currentColor"
+          class="bi bi-search"
+          viewBox="0 0 20 20"
+        >
+          <path
+            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+          />
+        </svg>
+        <input
+          type="text"
+          placeholder="지역 또는 아파트명을 입력하세요."
+          aria-label=".form-control-lg example"
+        />
+      </label>
+
+      </div>
+    </form>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#serchBar>div{
+  box-shadow: 0 2px 1px 0 rgb(0 0 0 / 15%);
+  
+}
+
+#searchBar label {
+  display: flex;
+  align-items: center;
+  width: 300px;
+  height: 50px;
+  margin: 0 25px;
+  padding: 0 0 0 10px;
+  border-radius: 3px;
+  background-color: white;
+}
+
+#searchBar input {
+  width: 100%;
+  height: 20px;
+  margin-left: 10px;
+  font-size: 15px;
+  border: none !important;
+  outline: none !important;
+  box-shadow: none !important;
+  appearance: none;
+}
+
+#searchBar input:focus {
+  background-color: white;
+}
+</style>
