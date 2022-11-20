7ec29a58981d2fe52e1b8072f5a58906b4259e11
kimahyeon <mauv2sky@gmail.com>
2022-11-20 16:12:03 +0900

    modify: Preview 컴포넌트 분리

--- src/components/PreView.vue
+++ src/components/PreView.vue
@@ -5,40 +5,13 @@
       <!-- section header -->
 
       <div class="row gy-5">
-        <div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-          <div class="post-item position-relative h-100">
-            <div class="post-img position-relative overflow-hidden">
-              <img src="assets/img/blog/blog-1.jpg" class="img-fluid" alt="" />
-              <span class="post-date">December 12</span>
-            </div>
-
-            <div class="post-content d-flex flex-column">
-              <h3 class="post-title">Eum ad dolor et. Autem aut fugiat debitis</h3>
-
-              <div class="meta d-flex align-items-center">
-                <div class="d-flex align-items-center">
-                  <i class="bi bi-person"></i> <span class="ps-2">Julia Parker</span>
-                </div>
-                <span class="px-3 text-black-50">/</span>
-                <div class="d-flex align-items-center">
-                  <i class="bi bi-folder2"></i> <span class="ps-2">Politics</span>
-                </div>
-              </div>
-
-              <hr />
-
-              <a href="blog-details.html" class="readmore stretched-link"
-                ><span>Read More</span><i class="bi bi-arrow-right"></i
-              ></a>
-            </div>
-          </div>
-        </div>
+        <PreviewUI></PreviewUI>
         <!-- End post item -->
 
         <div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
           <div class="post-item position-relative h-100">
             <div class="post-img position-relative overflow-hidden">
-              <img src="assets/img/blog/blog-2.jpg" class="img-fluid" alt="" />
+              <img src="assets/img/interior-2.jpg" class="img-fluid" alt="" />
               <span class="post-date">July 17</span>
             </div>
 
@@ -56,10 +29,11 @@
               </div>
 
               <hr />
-
-              <a href="blog-details.html" class="readmore stretched-link"
-                ><span>Read More</span><i class="bi bi-arrow-right"></i
-              ></a>
+              <div>
+                <a href="blog-details.html" class="readmore "
+                  ><span>Read More</span><i class="bi bi-arrow-right"></i
+                ></a>
+              </div>
             </div>
           </div>
         </div>
@@ -68,7 +42,7 @@
         <div class="col-xl-4 col-md-6">
           <div class="post-item position-relative h-100" data-aos="fade-up" data-aos-delay="300">
             <div class="post-img position-relative overflow-hidden">
-              <img src="assets/img/blog/blog-3.jpg" class="img-fluid" alt="" />
+              <img src="assets/img/interior-3.jpg" class="img-fluid" alt="" />
               <span class="post-date">September 05</span>
             </div>
 
@@ -101,10 +75,15 @@
 </template>
 
 <script>
-export default {};
+import PreviewUI from "@/components/PreViewUI.vue";
+export default {
+  components: {
+    PreviewUI
+  }
+};
 </script>
 
-<style>
+<style scoped>
 .row > * {
   padding-right: calc(1.5rem * .5);
   padding-left: calc(1.5rem * .5);
